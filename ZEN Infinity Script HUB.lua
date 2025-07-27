415cdb3279e7676198c63a7f0ac09b7e5a9cbca0
Infinite <217374376+Infinite827@users.noreply.github.com>
2025-07-26 23:13:40 -0400

    Update ZEN Infinity Script HUB.lua

--- ZEN Infinity Script HUB.lua
+++ ZEN Infinity Script HUB.lua
@@ -1,48 +1,30 @@
--- ZEN Infinity Script HUB
--- Uses Rayfield UI
 
+-- ZEN Infinity Script HUB
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+wait(0.5)
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+    Name = "ZEN Infinity Script HUB",
+    LoadingTitle = "ZEN Infinity Script HUB",
+    LoadingSubtitle = "by Infinite_Original",
+    ConfigurationSaving = {
+        Enabled = false,
+        FolderName = nil,
+        FileName = "ZENInfinityHubConfig"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "",
+        RememberJoins = false
+    },
+    KeySystem = false,
+    Theme = Rayfield.Themes.Amethyst
 })
+
+local Player = game.Players.LocalPlayer
 
 -- Home Tab
 local Home_Tab = Window:CreateTab("Home", 4483362458)
-local Player = game.Players.LocalPlayer
 Home_Tab:CreateLabel("Hi, " .. Player.DisplayName)
 Home_Tab:CreateDivider()
 
@@ -115,7 +97,6 @@ local function getTargets(arg)
     return {}
 end
 
--- Chat Commands
 local function sendChat(msg, color)
     game.StarterGui:SetCore("ChatMakeSystemMessage", {
         Text = msg,
@@ -123,7 +104,6 @@ local function sendChat(msg, color)
     })
 end
 
--- Track effects applied to local player for cleanup
 local activeEffects = {
     flying = false,
     spinning = false,
@@ -202,7 +182,6 @@ Player.Chatted:Connect(function(msg)
     end
 end)
 
--- Cleanup and kill local player on GUI close
 Window:OnClose(function()
     if activeEffects.spinning then
         local root = Player.Character and Player.Character:FindFirstChild("HumanoidRootPart")
@@ -227,3 +206,4 @@ Window:OnClose(function()
 end)
 
 Rayfield:Notify({Title = "ZEN Infinity Script HUB", Content = "Loaded successfully!", Duration = 5})
+
