ae8bbcbd0a041704a71846689e865af051ce3620
Infinite <217374376+Infinite827@users.noreply.github.com>
2025-07-27 00:34:57 -0400

    Update ZEN Infinity Script HUB.lua

--- ZEN Infinity Script HUB.lua
+++ ZEN Infinity Script HUB.lua
@@ -1,10 +1,10 @@
 
 -- Load Rayfield UI Library
 local success, Rayfield = pcall(function()
-    return loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua'))()
+    return loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 end)
 if success then
-print("Thanks for using our Script Hub")
+    print("Thanks for using our Script Hub")
 end
 else
 if not success then
@@ -47,10 +47,11 @@ local Window = Rayfield:CreateWindow({
 
 -- Home Tab
 local Home_Tab = Window:CreateTab("Home", 4483362458)
+local [display name] =
 local Player = game.Players.LocalPlayer
 local displayName = Player.DisplayName
 local Divider = Home_Tab:CreateDivider()
-local Paragraph = Home_Tab:CreateParagraph({Title = "", Content = "Hi , displayName
+local Paragraph = Home_Tab:CreateParagraph({Title = "", Content = "Hi ,[display name]
 ZEN Infinity Script HUB is the ultimate tool for chaos, laughs, and creative trolling in Roblox. Packed with powerful scripts, funny mods, and unpredictable effects, ZEN Infinity lets you bend the rules and mess with games in hilarious ways. From flying chairs to fake admin commands, it's all about having fun and confusing everyone around you. Easy to use, constantly updated, and loaded with trolling tools—ZEN Infinity is where the madness begins."})
 
 local Divider = Home_Tab:CreateDivider()
